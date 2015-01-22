38d62bc889a2728aa602bb814afc30405b40e94e
Gaban, Leon (lg209c) <leongaban@Leons-MBP.attlocal.net>
2015-01-21 20:38:18 -0600

    working on wallet data model

--- dashboard/_components/wallet.js
+++ dashboard/_components/wallet.js
@@ -8,6 +8,40 @@
 	var app = angular.module('app-wallet', [])
 	.controller('WalletCtrl', ['$scope', function($scope) {
 		
+		var vm = $scope;
+		vm.transactions = [
+			{
+				type: 'incoming',
+				comment: 'Recieved from multiple addresses',
+				time: '10 hours ago',
+				amount: 0.00498623
+			},
+			{
+				type: 'incoming',
+				comment: 'Recieve from 1MgZLyz6d8djEqe68XoPpsjx9BFQyVAtXN',
+				time: '12 hours ago',
+				amount: 0.003
+			},
+			{
+				type: 'outgoing',
+				comment: 'Sent to 17dPAMzZiosQYVty6ES4KSWN8R8XFcxShH',
+				time: 'Jan 15th 2015',
+				amount: 0.01
+			},
+			{
+				type: 'incoming',
+				comment: 'Recieved from multiple addresses',
+				time: 'Jan 14th 2015',
+				amount: 0.02874
+			},
+			{
+				type: 'outgoing',
+				comment: 'Sent to 1GS9E86Y3mhK7Qwm1vqvgCmpE5u6MMxPML',
+				time: 'Jan 12th 2015',
+				amount: 0.064904
+			}
+		];
+
 	}]);
 
 })();
