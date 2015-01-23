344deec50909f017862c9c046d687efab21d0668
Gaban, Leon (lg209c) <leongaban@Leons-MacBook-Pro.local>
2015-01-23 15:59:49 -0600

    fixed fadeIn animation bug on overlay

--- dashboard/_components/wallet.js
+++ dashboard/_components/wallet.js
@@ -14,6 +14,7 @@
 		// Modal vars:
 		var public_address ="17dPAMzZiosQYVty6ES4KSWN8R8XFcxShH";
 		var qr_code = "_assets/img/qrcode.png";
+		
 		var receive_html = '<div ng-show="modal_receive" class="modal_qr"><img src="'+qr_code+'"/></div><p ng-show="modal_receive" class="public_address">'+public_address+'</p>';
 		var send_html = '<div ng-show="modal_send" class="send_form"><div class="label_input_combo"><label for="to_input">Send to address</label><input id="to_input" class="form-input" type="text" placeholder=""></div><div class="label_input_combo"><div class="btn_usd noselect">USD</div><label for="amount_input" class="label_amount">Amount</label><input id="amount_input" class="form-input" type="text" placeholder=""></div><button class="btn btn_med btn_send_now">Send</button></div>';
 
