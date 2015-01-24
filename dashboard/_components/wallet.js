48c7f0b1f311b2791d0ad5b9b75ab8f2cafa50f4
Gaban, Leon (lg209c) <leongaban@Leons-MBP.attlocal.net>
2015-01-24 16:14:01 -0600

    working on directive

--- dashboard/_components/wallet.js
+++ dashboard/_components/wallet.js
@@ -6,15 +6,16 @@
 (function() {
 
 	var app = angular.module('app-wallet', [])
-	.controller('WalletCtrl', ['$scope', '$sce', function($scope, $sce) {
+	.controller('WalletCtrl', ['$scope', '$sce', '$compile', function($scope, $sce, $compile) {
 		
 		var vm = $scope;
 		// $sce = Strict Contextual Escaping
 
 		var public_address = '',
-			qr_code 	   = '';
+			qr_code 	   = '',
+			currency	   = 'USD';
 
-		var send_html = '<div ng-show="modal_send" class="send_form"><div class="label_input_combo"><label for="to_input">Send to address</label><input id="to_input" class="form-input" type="text" placeholder=""></div><div class="label_input_combo"><div class="btn_usd noselect">USD</div><label for="amount_input" class="label_amount">Amount</label><input id="amount_input" class="form-input" type="text" placeholder=""></div><button class="btn btn_med btn_send_now">Send</button></div>';
+		var send_html = '<div ng-show="modal_send" class="send_form"><div class="label_input_combo"><label for="to_input">Send to address</label><input id="to_input" class="form-input" type="text" placeholder=""></div><div class="label_input_combo"><div ng-click="switchCurrency()" class="btn_usd noselect">'+currency+'</div><label for="amount_input" class="label_amount">Amount</label><input id="amount_input" class="form-input" type="text" placeholder=""></div><button class="btn btn_med btn_send_now">Send</button></div>';
 
 		// Modal settings:
 		vm.$parent.modal 		 = false;
@@ -31,18 +32,28 @@
 					// Code to generate QR code
 					// Update receive_obj
 
-					vm.$parent.title 	  = "Your Public Address";
-					public_address   	  = "17dPAMzZiosQYVty6ES4KSWN8R8XFcxShH";
-					qr_code 		 	  = "_assets/img/qrcode.png";
+					vm.$parent.title = "Your Public Address";
+					public_address   = "17dPAMzZiosQYVty6ES4KSWN8R8XFcxShH";
+					qr_code 		 = "_assets/img/qrcode.png";
 
 					vm.$parent.modal_bind = $sce.trustAsHtml('<div ng-show="modal_receive" class="modal_qr"><img src="'+qr_code+'"/></div><p ng-show="modal_receive" class="public_address">'+public_address+'</p>');
 
 					break;
 
 				case 'send':
-					vm.$parent.title 	  = "Send Bitcoin";
+					vm.$parent.title = "Send Bitcoin";
 					vm.$parent.modal_bind = $sce.trustAsHtml(send_html);
+					// vm.$parent.modal_bind = $compile($sce.trustAsHtml(send_html));
 					break;
+			}
+		};
+
+		vm.$parent.switchCurrency = function() {
+			console.log('clicked');
+			if (currency === 'USD') {
+				currency = 'BTC';
+			} else {
+				currency === 'USD';
 			}
 		};
 
