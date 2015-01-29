844357831cd022d2bb8092330bcd83848e593a8c
Leon Gaban <lg209c@att.com>
2015-01-29 16:12:13 -0600

    working on login page validation

--- public/_components/login.js
+++ public/_components/login.js
@@ -8,6 +8,17 @@
 	var app = angular.module('app-login', [])
 	.controller('LoginCtrl', ['$scope', function($scope) {
 
+		var vm = $scope;
+
+		// Quick form submit          
+        vm.$parent.submitLoginForm = function(isValid) {
+
+            // check to make sure form is valid
+            if (isValid) {
+                alert('our form is amazing');
+            }
+
+        };
 	    
 	}]);
 
