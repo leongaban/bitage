45e19af3d55df4dcaa9914cea599798c3c9f10ab
Paulo Darocha <prochafilho@gmail.com>
2015-01-31 19:42:25 -0600

    location working

--- public/_components/register.js
+++ public/_components/register.js
@@ -6,20 +6,19 @@
 (function() {
 
   var app = angular.module('app-register', [])
-  .controller('RegisterCtrl', ['$scope','$http', function($scope, $http) {
+  .controller('RegisterCtrl', ['$scope','$http','$location', function($scope, $http, $location) {
 
       var vm = $scope;
-
+      var local = $location;
     // Quick form submit          
         vm.$parent.submitRegisterForm = function(isValid) {
 
-            // vm.$parent.formData = {};
-            var newUser = 'test';
             // check to make sure form is valid
             if (isValid) {
 
                 
                 // process the form
+
                 var request = $http({
                         method  : 'POST',
                         url     : '/signup',
@@ -27,6 +26,9 @@
                         headers : { 'Content-Type': 'application/x-www-form-urlencoded' }  // set the headers so angular passing info as form data (not request payload)
                     })
                     .success(function() {
+                      var locationHost = local.host();
+                      console.log(locationHost + '/dashboard');
+
                      
                     });
 
