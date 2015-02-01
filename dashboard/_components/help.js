2f78de9d07b24b374ee8d1b2c550a1c92cf06d63
Leon Gaban <lg209c@att.com>
2015-02-01 15:10:18 -0600

    added help view

---
+++ dashboard/_components/help.js
@@ -0,0 +1,16 @@
+/*global angular */
+/* =========================================
+   HELP Module
+   ========================================= */
+
+(function() {
+
+	var app = angular.module('app-help',['notification-directives'])
+
+	.controller('HelpCtrl', ['$scope', function($scope) {
+		
+		var vm = $scope;
+
+	}]);
+
+})();
