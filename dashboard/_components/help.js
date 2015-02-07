f0199e91b0351172b606d472f16743067204b99b
Leon Gaban <lg209c@att.com>
2015-02-06 19:18:24 -0600

    working on login form

--- dashboard/_components/help.js
+++ dashboard/_components/help.js
@@ -5,11 +5,19 @@
 
 (function() {
 
-	var app = angular.module('app-help',['notification-directives'])
+	var app = angular.module('app-help', ['notification-directives'])
 
-	.controller('HelpCtrl', ['$scope', function($scope) {
+	.controller('HelpCtrl', ['$scope', '$http', function($scope, $http) {
 
 		var vm = this;
+
+		var timeoutMsg = function() {
+			vm.$parent.notification = false;
+		};
+
+		$scope.$parent.closeMsg = function() {
+			$scope.$parent.notification = false;
+		};
 
 		// setup e-mail data with unicode symbols
 		// send user name, email and public address
@@ -21,13 +29,33 @@
 		    html: '<b>Hello world ✔</b>' // html body
 		};
 
+		var postHelpForm = function() {
+			console.log(vm.formData);
+
+			// process the form
+			// login data contains remember boolean
+			var request = $http({
+					method  : 'POST',
+					url     : '/help',
+					data    : $.param(vm.formData),
+					headers : { 'Content-Type': 'application/x-www-form-urlencoded' }
+				})
+				.success(function() {
+
+				});
+		};
+
 		// Quick form submit
 		vm.submitHelpForm = function(isValid) {
 
 			// check to make sure form is valid
 			if (isValid) {
 				alert('our form is amazing');
-				postLoginForm();
+				postHelpForm();
+				// Show notification
+				vm.$parent.message = 'Thanks! We will get back to you soon.';
+				vm.$parent.notification = true;
+				$timeout(timeoutMsg, 4000);
 			} else {
 				alert('Please correct the form');
 			}
