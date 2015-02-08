ddfc8d2fd7d14b96fee4c47f2c468507989b99d1
Leon Gaban <lg209c@att.com>
2015-02-07 22:11:58 -0600

    vm = this in accounts.js

--- dashboard/_components/accounts.js
+++ dashboard/_components/accounts.js
@@ -8,30 +8,32 @@
 	var app = angular.module('app-accounts', ['ngAnimate'])
 	.controller('AcctCtrl', ['$scope', function($scope) {
 
-		$scope.accounts = [];
-		$scope.accounts = [
+		var vm = this;
+
+		vm.accounts = [];
+		vm.accounts = [
 			{ type: 'Savings', label: 'BitAge', balance: '1.001', address: '16mCDhpziD6kBwPNnh1gSEHhdGFjAYYZdq' },
 			{ type: 'Savings', label: 'Blockchain.info', balance: '3.001', address: '17dPAMzZiosQYVty6ES4KSWN8R8XFcxShH' },
 			{ type: 'Savings', label: 'Coinbase wallet', balance: '0.562', address: '14TKW5r2EDhGPHsrsbPrbZq9ZXm96SP68W' },
 			{ type: 'Savings', label: 'Xapo wallet', balance: '0.003', address: '13sizB7zFU9wrxotFAVniG6cJBA9fXzhea' }
 		];
 
-		$scope.addAccount = function() {
+		vm.addAccount = function() {
 			// Don't add account if blank
-		    if ($scope.label === '' ||
-		    	$scope.label === undefined ||
-		    	$scope.address === undefined) { return; }
+		    if (vm.label === '' ||
+		    	vm.label === undefined ||
+		    	vm.address === undefined) { return; }
 
 		    // Add new account to accounts []
-		    $scope.accounts.push({
-				label: $scope.label,
+		    vm.accounts.push({
+				label: vm.label,
 				balance: 0,
-				address: $scope.address
+				address: vm.address
 		    });
 
 		    // Reset inputs
-		    $scope.label = '';
-		    $scope.address = '';
+		    vm.label = '';
+		    vm.address = '';
 		};
 
 		// select public addresses on click
@@ -44,8 +46,8 @@
 			    selection.addRange(range);
 		};
 
-		$scope.pubAddress = {};
-		$scope.pubAddress.getClick = function(the_id) {
+		vm.pubAddress = {};
+		vm.pubAddress.getClick = function(the_id) {
 			selectAddress(the_id);
 		};
 
