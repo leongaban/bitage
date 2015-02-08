63a6701c848aa83faad20bac3aa7b7e94dd73bba
Leon Gaban <lg209c@att.com>
2015-02-08 00:27:58 -0600

    created account service & directives

--- dashboard/_components/notificationDirectives.js
+++ dashboard/_components/notificationDirectives.js
@@ -10,12 +10,13 @@
 
 	    return {
 	        restrict: 'E',
-	        template: '<section ng-show="notification" ' +
-					  		   'ng-click="closeMsg()" ' +
-					  		   'class="ng-notification"> ' +
-					  	'<p class="notify-msg">{{message}}</p> ' +
-					  	'<div class="notify-bg success"></div> ' +
-					  '</section>'
+	        template: 
+	        	'<section ng-show="notification" ' +
+					'ng-click="closeMsg()" ' +
+					'class="ng-notification"> ' +
+					'<p class="notify-msg">{{message}}</p> ' +
+					'<div class="notify-bg success"></div> ' +
+				'</section>'
 	    };
 	});
 
