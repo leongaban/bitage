04ccf604100ada06999742256c9e2ebd7d370823
Paulo Darocha <prochafilho@gmail.com>
2015-02-17 20:23:51 -0600

    Decoupling Server and Client

--- server.js
+++ server.js
@@ -60,8 +60,7 @@ require('./app/routes.js')(app, passport);
 var dashboard = express.Router();
 app.use('/dashboard', dashboard);
 dashboard.get('/', function(req, res) {
-	console.log('returning dashboard index');
-	res.sendfile('dashboard/index.html');
+	res.sendfile('./dashboard/index.html');
 });
 
 
